0d39e9716b2566625b1271c66800f4f3fa218eb1
Austin deBruyn <austin.debruyn@gmail.com>
2017-12-05 22:30:09 -0800

    tidy up script

--- bin/touch-lynbot.sh
+++ bin/touch-lynbot.sh
@@ -1,6 +1,6 @@
 set -ex
-
-curl -vvv -X POST https://$LYNBOT_HOST/deploy \
-  -H 'Authorization: $LYNBOT_PASSWORD' \
-  -H 'Content-Type: application/json' \
-  -d '{"commit":"$TRAVIS_COMMIT","buildNUmber":"TRAVIS_BUILD_NUMBER"}'
+echo "$LYNBOT_PASSWORD"
+curl https://$LYNBOT_HOST/deploy \
+  -H "Authorization: $LYNBOT_PASSWORD" \
+  -H "Content-Type: application/json" \
+  -d "{\"commit\":\"$TRAVIS_COMMIT\",\"buildNumber\":\"$TRAVIS_BUILD_NUMBER\"}"
