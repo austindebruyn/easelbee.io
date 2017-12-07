c8e6203e8b90d341fc18bf0d89c635f52a342f05
Austin deBruyn <austin.debruyn@gmail.com>
2017-12-06 23:46:31 -0800

    lynbot api

--- api/lib/lynbotApi.js
+++ api/lib/lynbotApi.js
@@ -1,11 +1,30 @@
 const axios = require('axios');
+const config = require('../config');
 
 class LynbotAPI {
   constructor() {
 
   }
 
-  send(channel, message) {
-    
+  send(message) {
+    return new Promise((resolve, reject) => {
+      const {
+        protocol,
+        host,
+        port,
+        password
+      } = config.lynbot;
+
+      const body = { message };
+
+      return new axios.post(`${protocol}://${host}:${port}/post`, body, {
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': password
+        }
+      })
+        .then(resolve)
+        .catch(reject);
+    });
   }
 }
