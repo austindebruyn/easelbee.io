c8488881257cf578b8ca1242f06601943abcd944
Austin deBruyn <austin.debruyn@gmail.com>
2018-01-14 15:44:55 -0800

    Fix test

--- ui/src/components/forms/FormsList.test.js
+++ ui/src/components/forms/FormsList.test.js
@@ -5,20 +5,22 @@ import formsFixture from 'fixtures/forms';
 
 describe('FormsList', function () {
   it('should render zero data', function () {
-    const wrapper = shallow(FormsList, { propsData: {
-      forms: []
-    }});
+    const wrapper = shallow(FormsList, {
+      propsData: { forms: [] },
+      i18n: this.i18n
+    });
 
     expect(wrapper.find('.forms-list ul')).to.have.length(0);
-    expect(wrapper.first('.forms-list > p').text()).to.eql(
-      'No forms yet :)'
+    expect(wrapper.first('.forms-list .forms-zero-data-state').text()).to.eql(
+      'Nothing here!'
     );
   });
 
   it('should render list', function () {
-    const wrapper = shallow(FormsList, { propsData: {
-      forms: [ formsFixture.basic, formsFixture.basic2 ]
-    }});
+    const wrapper = shallow(FormsList, {
+      propsData: { forms: [ formsFixture.basic, formsFixture.basic2 ] },
+      i18n: this.i18n
+    });
 
     expect(wrapper.find('.forms-list ul')).to.have.length(1);
     expect(wrapper.find(FormsListItem)).to.have.length(2);
