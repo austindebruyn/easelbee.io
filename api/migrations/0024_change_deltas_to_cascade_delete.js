55f4297cd16e3a5229b5d08b0f664b14f89c6d8c
Austin deBruyn <austin.debruyn@gmail.com>
2018-03-24 11:47:29 -0700

    Should have linted

--- api/migrations/0024_change_deltas_to_cascade_delete.js
+++ api/migrations/0024_change_deltas_to_cascade_delete.js
@@ -2,10 +2,10 @@ module.exports = {
   up: function (q, Sequelize) {
     return q.sequelize.query('ALTER TABLE deltas DROP FOREIGN KEY deltas_ibfk_1')
       .then(() => {
-        return q.sequelize.query('ALTER TABLE deltas DROP KEY optionId')
+        return q.sequelize.query('ALTER TABLE deltas DROP KEY optionId');
       })
       .then(() => {
         return q.sequelize.query('ALTER TABLE deltas ADD CONSTRAINT deltas_ibfk_1 FOREIGN KEY (`optionId`) REFERENCES `options` (`id`) ON DELETE CASCADE');
       });
   }
-}
+};
